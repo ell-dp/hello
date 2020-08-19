32073a22d3d0b8603d8cd34ef67b3255212e03d7
nnelson15 <69835724+nnelson15@users.noreply.github.com>
2020-08-19 14:05:19 -0500

    Update hello.c
    added collaborator name/major

--- hello.c
+++ hello.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv) {
   printf("Ellen Diep!!!\n");
   printf("Computer Engineer Major!\n");
   printf("Nick Nelson.\n");
-  printf("Undeclared\n");
+  printf("Undeclared.\n");
 
   return 0;
 }
